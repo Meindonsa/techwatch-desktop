8e79380118d76a404f2a4cf70884fee400f813a9
borisaxel <boris.ebaneth@ossas-it.com>
2026-04-16 06:16:27 +0100

    feat: Implement register

---
+++ src/shared/components/MyTransition.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps(['name', 'mode'])
+</script>
+
+<template>
+  <Transition :name="name" :mode="mode">
+    <slot></slot>
+  </Transition>
+</template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
